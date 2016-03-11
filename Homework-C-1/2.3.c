24e1324cbc138b67554d4d79ac1e685f9037f8ce
Aleksandr07 <a-katushnii@yandex.ru>
2016-03-11 22:33:26 +0300

    домашнее задание

---
+++ Homework-C-1/2.3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int day;
+	int month;
+	day=07;
+	month=04;
+	day=day+month;
+	month=day-month;
+	day=day-month;
+	printf("day=%d, month=%d \n", day, month);
+	return 0;
+}

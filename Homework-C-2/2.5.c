2ec80aa121918b6b7348034ae7daa41272f585d1
Aleksandr07 <a-katushnii@yandex.ru>
2016-03-15 21:00:09 +0300

    д

---
+++ Homework-C-2/2.5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+float a, b, sum, raz, proiz, chast;
+printf("Введите первое число \n");
+scanf("%f", &a);
+printf("Введите второе число \n");
+scanf("%f", &b);
+sum = a+b;
+raz = a-b;
+proiz = a*b;
+chast = a/b;
+printf("Сумма = %.2f, разность = %.2f, произведение = %.2f, частное = %.2f. \n", sum, raz, proiz, chast);
+return 0;
+}
